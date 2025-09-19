70ed9d4cb1cc5c202acfef0e9c93983798ed50e9
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2025-09-19 13:00:07 +0000

    feature: churn fast api handler

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # импортируем библиотеку для работы со случайными числами
 import random
+import catboost
 
 # импортируем класс для создания экземпляра FastAPI-приложения
 from fastapi import FastAPI
@@ -29,3 +30,24 @@ def get_approval():
     if score > 0.8:
         return {"approved": 1}
     return {"approved": 0}
+
+from catboost import CatBoostClassifier
+
+
+def load_churn_model(model_path: str):
+    """Загружаем обученную модель оттока.
+    Args:
+        model_path (str): Путь до модели.
+    """
+    try:
+        # ваш код здесь — загрузите модель
+        model = CatBoostClassifier()
+        model.load_model(model_path)
+        print("Model loaded successfully")
+    except Exception as e:
+        print(f"Failed to load model: {e}")
+    return model
+
+if __name__ == "__main__":
+    model = load_churn_model("models/catboost_churn_model.bin")
+    print(f"Model parameter names: {model.feature_names_}") 
